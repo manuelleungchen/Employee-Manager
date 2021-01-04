ba27f4bd0a4bd65ad9727eab44e32afb29588776
Manuel Leung Chen <manuel.leungchen@gmail.com>
2021-01-03 19:18:47 -0500

    Adding Role and Employee tables

--- seed.sql
+++ seed.sql
@@ -4,7 +4,25 @@ CREATE DATABASE employeeManagerDB;
 
 USE employeeManagerDB;
 
-CREATE TABLE department {
-    id INT PRIMARY KEY,
-    name VARCHAR(30)
-}
+CREATE TABLE department (
+    id INT NOT NULL,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL,
+    title VARCHAR(30) ,
+    salary DECIMAL(6,2),
+    department_id INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT DEFAULT null,
+    PRIMARY KEY (id)
+);
