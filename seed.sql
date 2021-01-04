da8a983767856d0c08fdb28f6b921d30c9be7d4f
Manuel Leung Chen <manuel.leungchen@gmail.com>
2021-01-03 20:11:24 -0500

    Linked Tables with foreigh keys

--- seed.sql
+++ seed.sql
@@ -15,7 +15,8 @@ CREATE TABLE role (
     title VARCHAR(30) ,
     salary DECIMAL(6,2),
     department_id INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -24,5 +25,6 @@ CREATE TABLE employee (
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT DEFAULT null,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id)
 );
