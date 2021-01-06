f0de247f1aa312a6e9d0a3bcec6a9127ffd10f0a
Manuel Leung Chen <manuel.leungchen@gmail.com>
2021-01-06 18:36:23 -0500

    Splitted seed.sql into two files

---
+++ schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employeeManagerDB;
+
+CREATE DATABASE employeeManagerDB;
+
+USE employeeManagerDB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) ,
+    salary DECIMAL(10,2),
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT DEFAULT null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id)
+);
